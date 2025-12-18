6003964813c2a752c1270e41d43e3b2d6d947225
ertaz <ez70509@ubt-uni.net>
2025-12-18 21:51:56 +0100

    Shtimi i funksioneve për doctors në API

--- backend/api-gateway/server.js
+++ backend/api-gateway/server.js
@@ -1,11 +1,29 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
+app.use(cors());
 
-app.use('/auth', createProxyMiddleware({ target: 'http://localhost:5000', changeOrigin: true, pathRewrite: {'^/auth': '/api/auth'} }));
-app.use('/patients', createProxyMiddleware({ target: 'http://localhost:5001', changeOrigin: true, pathRewrite: {'^/patients':'/api/patients'} }));
-app.use('/doctors', createProxyMiddleware({ target: 'http://localhost:5002', changeOrigin: true, pathRewrite: {'^/doctors':'/api/doctors'} }));
+// AUTH SERVICE
+app.use('/auth', createProxyMiddleware({
+  target: 'http://localhost:5000/api/auth',
+  changeOrigin: true
+}));
 
-app.listen(4000, () => console.log('API Gateway listening on 4000'));
+// PATIENT SERVICE
+app.use('/patients', createProxyMiddleware({
+  target: 'http://localhost:5001/api/patients',
+  changeOrigin: true
+}));
+
+//DOCTOR SERVICE
+app.use('/doctors', createProxyMiddleware({
+  target: 'http://localhost:5002/api/doctors',
+  changeOrigin: true
+}));
+
+app.listen(4000, () => {
+  console.log('API Gateway listening on 4000');
+});
